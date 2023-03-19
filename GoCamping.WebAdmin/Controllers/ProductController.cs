b6b4a05d190f443a49cfb98dd068688acde09e80
{name} <{emailaddress}>
2023-03-20 00:54:05 +0400

    ui ux done next step is data access layer

---
+++ GoCamping.WebAdmin/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoCamping.WebAdmin.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GoCampingWebUI/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoCamping.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
